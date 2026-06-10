1157dccbe2990e6e1319e099e9fc11a0d5468efa
uasr208 <arunsinghutkarsh10@gmail.com>
2026-06-10 19:43:35 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Lora, DM_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  style: ["normal", "italic"],
+  variable: "--font-serif",
+});
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Build It- AI App Builder",
+  description:
+    "build your idea into fully working app from scratch with ai app builder",
+  icons: "/logo-short.jpeg",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${lora.variable} ${dmSans.variable} font-sans`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main>{children}</main>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="pt-40">
+      <Button>Shri Ganeshay Namh</Button>
+    </div>
+  );
+}
